f250988f176e29b0bfdd1bfff7f853fb887abed9
Prohor722 <prohor.b722@gmail.com>
2022-06-09 03:48:16 +0600

    student added

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,13 @@ use Illuminate\Support\Facades\Route;
     return view('welcome');
 });*/
 
+//Common Routes
 Route::get('/login', 'App\Http\Controllers\LoginController@index')->name('login.index');
 Route::post('/login', 'App\Http\Controllers\LoginController@verify')->name('login.verify');
-
 Route::get('/logout', 'App\Http\Controllers\LogoutController@index')->name('logout.index');
 
+
+//Admin Routes
 Route::get('/admin', 'App\Http\Controllers\AdminController@index')->name('admin.index');
 Route::view('/admin/students', 'admin.studentList')->name('admin-students');
 Route::view('/admin/teachers', 'admin.teachers')->name('admin-teachers');
@@ -29,11 +31,15 @@ Route::view('/admin/addTeacher', 'admin.addTeacher')->name('add-teacher');
 Route::view('/admin/addStudent', 'admin.addStudent')->name('add-student');
 Route::view('/admin/marks', 'admin.marks')->name('admin-marks');
 
+//Teacher Routes
 Route::get('/teacher', 'App\Http\Controllers\TeacherController@index')->name('teacher.index');
 Route::view('/teacher/studentList', 'teacher.studentList')->name('teacher-student-list');
 Route::view('/teacher/groups', 'teacher.groups')->name('teacher-groups');
 Route::view('/teacher/tasks', 'teacher.tasks')->name('teacher-tasks');
 
+//Student Routes
 Route::get('/student', 'App\Http\Controllers\StudentController@index')->name('student.index');
+Route::view('/student/notice', 'student.notice')->name('student-notice');
+Route::view('/student/tasks', 'student.notice')->name('student-tasks');
 
 
